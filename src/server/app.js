200396a3669d670b45ffea1c65c045c255767a7a
Patel <patelde@c02xhq5qjg5j.analytics.moodys.net>
2019-11-18 15:26:35 -0800

    works now

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,8 @@
 import express from "express";
-import path from "path";
+// import path from "path";
 import logger from "morgan";
 import bodyParser from "body-parser";
-import pathRegexp from "path-to-regexp";
+// import pathRegexp from "path-to-regexp";
 import flash from "connect-flash";
 // import setupWebpackHotReload from "server/middleware/setupWebpackHotReload";
 // require("util").inspect.defaultOptions.depth = null;
@@ -28,37 +28,13 @@ app.use("/public", express.static("public"));
 // app.use("/api", require("server/middleware/api").default);
 // app.use("/api/v1", require("server/middleware/externalAPI/v1").default);
 
-console.log("dlksafjlksjfa");
-
 if (process.env.NODE_ENV === "development") {
   setupWebpackHotReload(app);
 }
 // app.use("/", require("server/middleware/web").default);
 
 export default app;
-console.log("MODULE IS NOT HOT");
+
 if (module.hot) {
   module.hot.accept();
-  console.log("MODULE IS HOT");
-  const routeMatchesPath = (route, path) => {
-    return (
-      route.name === "router" &&
-      route.regexp.toString() ===
-        pathRegexp(path, [], { end: false }).toString()
-    );
-  };
-  module.hot.accept("server/middleware/api", () => {
-    app._router.stack.forEach(route => {
-      if (routeMatchesPath(route, "/api")) {
-        route.handle = require("server/middleware/api").default;
-      }
-    });
-  });
-  module.hot.accept("server/middleware/health", () => {
-    app._router.stack.forEach(route => {
-      if (routeMatchesPath(route, "/health")) {
-        route.handle = require("server/middleware/health").default;
-      }
-    });
-  });
 }
--- src/server/middleware/api/index.js
+++ src/server/middleware/api/index.js
@@ -1,5 +1,3 @@
 // import { path } from "ramda";
 
 // import wrapAsync from "server/middleware/wrapAsync";
-
-console.log("dasf");
